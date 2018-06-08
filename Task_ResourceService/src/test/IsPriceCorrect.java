7300a196c5230cd166d0f3c14343f0a4151b2e23
Ann-Kathrin <annkathrin.m96@googlemail.com>
2018-06-08 13:42:49 +0200

    1234

--- Task_ResourceService/src/test/IsPriceCorrect.java
+++ Task_ResourceService/src/test/IsPriceCorrect.java
@@ -2,7 +2,6 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
