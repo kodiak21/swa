0e9e1cd729cd97cdc0026e5ad0cfb3aa2dc1c367
alyphant <dnlschulz86@gmail.com>
2018-06-08 14:02:24 +0200

    Alles wieder clean

--- Task_Authenfication/src/de/leuphana/swa/authentificationService/CredentialType.java
+++ Task_Authenfication/src/de/leuphana/swa/authentificationService/CredentialType.java
@@ -2,19 +2,4 @@ package de.leuphana.swa.authentificationService;
 
 public enum CredentialType {
 	USERNAME, FINGERPRINT, EYESCAN;
-	// Ach gott
-	//neu
-	
-	//hi
-	//neuster Kommentar
-	//hallo123
-	//Hello Friends
 }
-
-//Kein Gott 123456789
-//Wusel Dusel
-//Ich mag kein GitHub
-//ich auch nicht
-
-//Neuer branch1
-//Ich bin da
