9cccfb3c10f5f8eac0e468c105078ceb12076569
alyphant <dnlschulz86@gmail.com>
2018-06-15 10:52:42 +0200

    Das Muster sauberer umgesetzt, redundanten Code entfernt

--- Task_PaymentService/src/de/leuphana/swa/paymentService/MoneyWalletPayment.java
+++ Task_PaymentService/src/de/leuphana/swa/paymentService/MoneyWalletPayment.java
@@ -2,24 +2,6 @@ package de.leuphana.swa.paymentService;
 
 public class MoneyWalletPayment extends Payment {
 
-	@Override
-	void authenficateUser(CredentialType credentialType) {
-		System.out.print("User is identified ");
-		switch(credentialType) {
-		case USERNAME:
-			System.out.print("via Username\n");
-			break;
-		case FINGERPRINT:
-			System.out.print("via fingerprint\n");
-			break;
-		case EYESCAN:
-			System.out.print("via eyescan\n");
-			break;
-		default:
-			break;
-		}
-		
-	}
 
 	@Override
 	void transferMoney(Account sender, Account receiver, float amount) {
@@ -28,12 +10,6 @@ public class MoneyWalletPayment extends Payment {
 				+ sender.getAccountNumber() 
 				+ " to "
 				+ receiver.getAccountNumber());
-		
-	}
-
-	@Override
-	void confirmPayment() {
-		System.out.println("transaction completed !");
 		
 	}
 
--- Task_PaymentService/src/de/leuphana/swa/paymentService/Payment.java
+++ Task_PaymentService/src/de/leuphana/swa/paymentService/Payment.java
@@ -1,22 +1,36 @@
 package de.leuphana.swa.paymentService;
 
+import de.leuphana.swa.authentificationService.AuthenficationMethod;
+import de.leuphana.swa.authentificationService.AuthentificationMethodFactory;
+import de.leuphana.swa.authentificationService.Subject;
+import de.leuphana.swa.authentificationService.CredentialType;
+
 public abstract class Payment {
 	
-	private float amount;
-	private Account sender;
-	private Account receiver;
-
-	abstract void authenficateUser(CredentialType credentialType);
+	private boolean authenficateUser(CredentialType credentialType, Account senderAccount) {
+		boolean authentificatet;
+		Subject subject;
+		
+		//TODO muss noch variabel gestaltet werden, Einbindung von Authentification Subject bzw PersonService-PErson
+		subject = Subject.NATURALPERSON;
+		
+		AuthenficationMethod authenficationMethod = AuthentificationMethodFactory.getAuthenticationMethod(credentialType, subject);
+		authentificatet = authenficationMethod.authenficateSubject();
+		
+		return authentificatet;
+	};
 	abstract void transferMoney(Account sender, Account receiver, float amount);
-	abstract void confirmPayment();
+	private void confirmPayment() {
+		System.out.println("transaction completed !");
+	}
 	
-	public final void payAmount(Account sender, Account receiver, float amount, CredentialType credentialType) {
+	public final void payAmount(Account senderAccount, Account receiverAccount, float amount, CredentialType credentialType) {
 		
 		//authenficate User
-		authenficateUser(credentialType);
+		authenficateUser(credentialType, senderAccount);
 		
 		//transfer money
-		transferMoney(sender, receiver, amount);
+		transferMoney(senderAccount, receiverAccount, amount);
 		
 		//confirm payment
 		confirmPayment();
