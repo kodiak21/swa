f86be0567c2124c77d9468f744a73146f8c47d71
kodiak21 <hans.o.kaltschmidt@gmail.com>
2018-07-07 11:02:43 +0200

    Command files added

---
+++ Task_OrganisationService/src/de/leuphana/swa/organisationService/commandpattern/realisations/AddGroupToOrganisationCommand.java
@@ -0,0 +1,109 @@
+package de.leuphana.swa.organisationService.commandpattern.realisations;
+
+import java.io.IOException;
+
+import de.leuphana.swa.organisationService.UserGroup;
+import de.leuphana.swa.organisationService.UserOrganisationUnit;
+import de.leuphana.swa.organisationService.commandpattern.OrganisationCommand;
+import de.leuphana.swa.personService.Person;
+
+public class AddGroupToOrganisationCommand implements OrganisationCommand {
+	
+	private UserOrganisationUnit orgUnit;
+	private UserGroup userGroup;
+	
+	public AddGroupToOrganisationCommand(final UserOrganisationUnit orgUnit, UserGroup userGroup) {	
+		this.orgUnit = orgUnit;
+		this.userGroup = userGroup;
+	}
+	
+	
+	@Override
+	public void addUserToGroup() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addGroupToOrganisation() {
+		orgUnit.add(userGroup);	
+	}
+
+
+	@Override
+	public void printUserInformation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void deleteUserFromGroup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void deleteGroupFromOrganisation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public Person createPerson() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Person removePerson() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public String askGroupName() {
+		return null;
+		
+	}
+
+
+	@Override
+	public String askOrgUnitName() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public int shouldPersonAddedGroup() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int shouldPersonRemovedGroup() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int shouldGroupAddedOrgUnit() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int shouldGroupRemovedOrgUnit() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Task_OrganisationService/src/de/leuphana/swa/organisationService/commandpattern/realisations/CreatePersonCommand.java
@@ -0,0 +1,111 @@
+package de.leuphana.swa.organisationService.commandpattern.realisations;
+
+import java.io.IOException;
+
+import de.leuphana.swa.organisationService.commandpattern.OrganisationCommand;
+import de.leuphana.swa.personService.Person;
+import de.leuphana.swa.personService.PersonType;
+import de.leuphana.swa.personService.UserFactory;
+
+public class CreatePersonCommand implements OrganisationCommand {
+
+	private UserFactory userFactory;
+	private PersonType personType;
+	private String name;
+	
+	public CreatePersonCommand(final UserFactory userFactory, PersonType personType, String name) {
+		this.userFactory = userFactory;
+		this.personType = personType;
+		this.name = name;
+	}
+	
+
+	@Override
+	public Person createPerson(){
+		Person p = userFactory.createPerson(personType, name);
+		return p;
+	}
+
+
+	@Override
+	public void addUserToGroup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void addGroupToOrganisation(){
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void printUserInformation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void deleteUserFromGroup()  {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void deleteGroupFromOrganisation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public Person removePerson() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public String askGroupName() {
+		return null;
+		
+	}
+
+
+	@Override
+	public String askOrgUnitName() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public int shouldPersonAddedGroup() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int shouldPersonRemovedGroup() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int shouldGroupAddedOrgUnit() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int shouldGroupRemovedOrgUnit() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
