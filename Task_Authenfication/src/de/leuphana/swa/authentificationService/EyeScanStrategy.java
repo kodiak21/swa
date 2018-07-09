65e00476d7371d71722027f574fe14d5bd9fbe54
alyphant <dnlschulz86@gmail.com>
2018-07-09 13:03:06 +0200

    vorbereitung auf pull von Olli

--- Task_Authenfication/src/de/leuphana/swa/authentificationService/EyeScanStrategy.java
+++ Task_Authenfication/src/de/leuphana/swa/authentificationService/EyeScanStrategy.java
@@ -2,11 +2,11 @@ package de.leuphana.swa.authentificationService;
 
 public class EyeScanStrategy implements AuthenficationStrategy {
 	
-	String text = "Idenfified by EyeScan";
+//	String text = "Idenfified by EyeScan";
 	
 	public boolean authenficateSubject() {
 		boolean isAuthenficated = true;
-		System.out.println(text);
+//		System.out.println(text);
 		return isAuthenficated;
 	}
 }
