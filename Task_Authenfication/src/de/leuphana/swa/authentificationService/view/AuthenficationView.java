8fc2ec1bd02452dcb3bf9366b5ec06fbe0046dca
alyphant <dnlschulz86@gmail.com>
2018-07-10 11:25:09 +0200

    Minimale änderungen am Authentification View

--- Task_Authenfication/src/de/leuphana/swa/authentificationService/view/AuthenficationView.java
+++ Task_Authenfication/src/de/leuphana/swa/authentificationService/view/AuthenficationView.java
@@ -13,18 +13,20 @@ public class AuthenficationView {
 
 	public int printAuthentificateChoice(PersonType personType, String name) throws IOException {
 
+		int answer = 0;
+		
 		System.out.println("Person with name: " + name + " should be identified by");
 		if(personType==PersonType.NATURALPERSON) {
 			System.out.println("1: Username");
 			System.out.println("2: Eyescan");
 			System.out.println("3: Fingerprint");
+			answer = Integer.parseInt(br.readLine());
 		}else if(personType==PersonType.LEGALPERSON) {
-			System.out.println("1: Username");
+			System.out.println("Username");
+			answer = 1;
 		}
 		
-		int i = Integer.parseInt(br.readLine());
-		
-		return i;
+		return answer;
 	}
 	
 	public void printAuthentificationStrategy(AuthenficationStrategy method) {
