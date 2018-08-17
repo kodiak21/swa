31c528758e88dfbf8e05b9ee841728fec8d22840
kodiak21 <hans.o.kaltschmidt@gmail.com>
2018-08-17 22:23:21 +0200

    isAuthentificated gibt richtigen Wert zurück

--- Task_Authenfication/src/model/UserNameStrategy.java
+++ Task_Authenfication/src/model/UserNameStrategy.java
@@ -3,60 +3,56 @@ package model;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class UserNameStrategy implements AuthentificationStrategy{
-	
+public class UserNameStrategy implements AuthentificationStrategy {
+
 	private String password;
-	
+	private boolean isAuthenficated;
+
 	public UserNameStrategy(String password) {
 		this.password = password;
+		this.isAuthenficated = false;
 	}
 
 	@Override
 	public boolean authenficateSubjectGer() {
 		String inputPassword;
-		boolean isAuthenficated = false;
-		
-		System.out.println("Geben Sie Ihr Passwort ein:");
-        try
-        {
-            InputStreamReader in = new InputStreamReader(System.in);
-            BufferedReader br = new BufferedReader(in);
-            inputPassword = br.readLine();
-    		if (inputPassword.equals(password)) {
-    			isAuthenficated = true;
-    		} else {
-    			System.out.println("Falsches Passwort");
-    			authenficateSubjectGer();
-    			}
-        }
-        catch(Exception e){
-        }
 
-		return isAuthenficated;	
+		System.out.println("Geben Sie Ihr Passwort ein:");
+		try {
+			InputStreamReader in = new InputStreamReader(System.in);
+			BufferedReader br = new BufferedReader(in);
+			inputPassword = br.readLine();
+			if (inputPassword.equals(password)) {
+				isAuthenficated = true;
+			} else {
+				System.out.println("Falsches Passwort");
+				authenficateSubjectGer();
+			}
+		} catch (Exception e) {
+		}
+
+		return isAuthenficated;
 	}
-	
+
 	@Override
 	public boolean authenficateSubjectEng() {
 		String inputPassword;
-		boolean isAuthenficated = false;
-		
-		System.out.println("Enter your password:");
-        try
-        {
-            InputStreamReader in = new InputStreamReader(System.in);
-            BufferedReader br = new BufferedReader(in);
-            inputPassword = br.readLine();
-    		if (inputPassword.equals(password)) {
-    			isAuthenficated = true;
-    		} else {
-    			System.out.println("Wrong password!");
-    			authenficateSubjectEng();
-    			}
-        }
-        catch(Exception e){
-        }
 
-		return isAuthenficated;	
+		System.out.println("Enter your password:");
+		try {
+			InputStreamReader in = new InputStreamReader(System.in);
+			BufferedReader br = new BufferedReader(in);
+			inputPassword = br.readLine();
+			if (inputPassword.equals(password)) {
+				isAuthenficated = true;
+			} else {
+				System.out.println("Wrong password!");
+				authenficateSubjectEng();
+			}
+		} catch (Exception e) {
+		}
+
+		return isAuthenficated;
 	}
 
 	@Override
